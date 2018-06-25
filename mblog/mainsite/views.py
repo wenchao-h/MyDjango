c00bf1f9f184b93da45b56b4b1965b47449fee6c
wenchao-h <wenchao_h@outlook.com>
2018-06-25 20:23:39 +0800

    models.py urls.py views.py

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -2,5 +2,20 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Post
 # Create your views here.
+
+def homepage(request):
+    posts = Post.objects.all()
+    print(posts)
+    print(type(posts))
+    post_lists = list()
+    for count, post in enumerate(posts):
+        print(type(count))
+        print(type(post))
+        print(post)
+        post_lists.append("No.{}:".format(str(count)) + str(post) + "<br>")
+    print(post_lists)
+    return HttpResponse(post_lists)
+
