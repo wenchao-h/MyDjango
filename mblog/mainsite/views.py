5bc2dc8b60d64054bc0a73f576309407345a4101
wenchao-h <wenchao_h@outlook.com>
2018-06-26 12:02:39 +0800

    templates/index.html

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.template.loader import get_template
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 # Create your views here.
 
-def homepage(request):
-    posts = Post.objects.all()
-    print(posts)
-    print(type(posts))
-    post_lists = list()
-    for count, post in enumerate(posts):
-        print(type(count))
-        print(type(post))
-        print(post)
-        post_lists.append("No.{}:".format(str(count)) + str(post) + "<br>")
-    print(post_lists)
-    return HttpResponse(post_lists)
+#def homepage(request):
+#    posts = Post.objects.all()
+#    post_lists = list()
+#    for count, post in enumerate(posts):
+#        post_lists.append("No.{} ".format(str(count)) +post.title+"<hr>")
+#        post_lists.append("<small>" + post.body + "</small><br></br>")        
+#    return HttpResponse(post_lists)
 
+def homepage(request):
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
